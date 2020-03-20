9160d7fa23b7fd0455b453498d8eb403f44a1248
Guillermo Manterola Rojas <guillermo.manterola@raona.com>
2020-03-20 18:40:57 +0100

    mejorado

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {service} from './service';
 import { HttpClientModule } from '@angular/common/http';
 import { mensaje } from './Mensaje';
 import * as big from 'bigint-crypto-utils'
-import * as rsa from '../../../../rsa/rsa-cybersecurity';
+import * as rsa from '../../../../rsa2/rsa-cybersecurity';
 import * as bigconv from 'bigint-conversion'
 
 @Component({
@@ -22,9 +22,9 @@ export class AppComponent {
    }
    
    respuesta: any;
-   publicKey: any;
-   privateKey: any;
-   serverPublicKey: any;
+   publicKey: rsa.PublicKey;
+  privateKey: rsa.PrivateKey;
+  serverPublicKey: rsa.PublicKey;
    mensajeConsola: any;
    mensaje: any;
 
@@ -45,14 +45,16 @@ export class AppComponent {
   }
 
   enviarMensaje() {
-    this.mensajeConsola = "Hola servidor"
-    this.mensaje = new mensaje(bigconv.bigintToHex(rsa.encrypt(bigconv.textToBigint(this.mensajeConsola), this.serverPublicKey[0], this.serverPublicKey[1])), bigconv.bigintToHex(this.publicKey.e), bigconv.bigintToHex(this.publicKey.n))
+    this.mensajeConsola = "Hola servidor!"
+    console.log(this.mensajeConsola);
+    this.mensaje = new mensaje(bigconv.bigintToHex(this.serverPublicKey.encrypt(bigconv.textToBigint(this.mensajeConsola))),bigconv.bigintToHex(this.publicKey.e), bigconv.bigintToHex(this.publicKey.n))
+
     this.Service.enviarMe(this.mensaje)
       .subscribe((res: any) => {
-        this.respuesta = bigconv.bigintToText(rsa.decrypt(bigconv.hexToBigint(res.respuestaServidor), this.privateKey.d, this.privateKey.publicKey.n));
+        this.respuesta = bigconv.bigintToText(this.privateKey.decrypt(bigconv.hexToBigint(res.respuestaServidor)))
         console.log(this.respuesta);
-      })
-    }
+      });
+  }
 
   async claves() {
     const { publicKey, privateKey } = await rsa.generateRandomKeys(3072);
@@ -60,11 +62,9 @@ export class AppComponent {
     this.privateKey = privateKey;
   }
 
-
   dameClave() {
     this.Service.dameClave().subscribe((res: any) => {
-      this.serverPublicKey = [bigconv.hexToBigint(res.e), bigconv.hexToBigint(res.n)];
-      this.enviarMensaje();
+      this.serverPublicKey = new rsa.PublicKey(bigconv.hexToBigint(res.e),bigconv.hexToBigint(res.n))
     })
   }
 }
